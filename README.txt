4b6b2605d72340decee705d77736bf477994342c
Kris <swartzbaugh.k@gmail.com>
2021-09-16 09:07:30 -0700

    Initial commit

---
+++ README.txt
@@ -0,0 +1,40 @@
+Summary:
+	Build general search algorithms and apply them to Pacman scenarios as he  attempts to nagivate his maze world. 
+
+
+
+Project Requirements:
+	
+	
+Project Requirements:
+	search.py algorithms will be placed here 
+	pacman.py - The main file that runs Pacman games. This file describes a Pacman Game State type, which you use in this project.
+	game.py - The logic behind how the Pacman world works. This file describes several
+supporting types like Agent State, Agent, Direction, and Grid.
+	util.py Useful data structures for implementing search algorithms.
+	searchAgents.py A variety of different types of Pacman agents. test_cases/ Directory containing the test cases for each question
+	
+	
+	(1) Finding a Fixed food dot using depth search 
+		implement the depth-first search (DFS) algorithm in the depthFirstSearch function in search.py 
+		Your code should quickly find a solution for:
+			python3.6 pacman.py -l tinyMaze -p SearchAgent
+			python3.6 pacman.py -l mediumMaze -p SearchAgent
+			python3.6 pacman.py -l bigMaze -z .5 -p SearchAgent
+		
+		
+		eg. python pacman.py -l tinyMaze -p SearchAgent -a fn=tinyMazeSearch
+			above command tells pacman to use tinyMazeSearch as the search agent to find the dot. 
+			Search nodes must contain a state but also the information necessary to reconstruct the path (plan) which gets to that state
+			Search functions must return a list of actions that will lead the agent from the start to the goal. Actions must be all legal moves (valid directions, no moving through walls)
+			use stack, queue, and PriorityQueue data structures provided in util.py (for autograder comtatability)
+			
+			
+	(2) Question 2: Breadth first Search 
+		breadthFirstSearch function in search.py
+		test code:
+			python3.6 pacman.py -l mediumMaze -p SearchAgent -a fn=bfs
+			python3.6 pacman.py -l bigMaze -p SearchAgent -a fn=bfs -z .5
+			
+			
+			
---
+++ search.py
@@ -0,0 +1,221 @@
+# search.py
+# ---------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+#
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+In search.py, you will implement generic search algorithms which are called by
+Pacman agents (in searchAgents.py).
+"""
+
+import util
+
+class SearchProblem:
+    """
+    This class outlines the structure of a search problem, but doesn't implement
+    any of the methods (in object-oriented terminology: an abstract class).
+
+    You do not need to change anything in this class, ever.
+    """
+
+    def getStartState(self):
+        """
+        Returns the start state for the search problem.
+        """
+        util.raiseNotDefined()
+
+    def isGoalState(self, state):
+        """
+          state: Search state
+
+        Returns True if and only if the state is a valid goal state.
+        """
+        util.raiseNotDefined()
+
+    def getSuccessors(self, state):
+        """
+          state: Search state
+
+        For a given state, this should return a list of triples, (successor,
+        action, stepCost), where 'successor' is a successor to the current
+        state, 'action' is the action required to get there, and 'stepCost' is
+        the incremental cost of expanding to that successor.
+        """
+        util.raiseNotDefined()
+
+    def getCostOfActions(self, actions):
+        """
+         actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.
+        The sequence must be composed of legal moves.
+        """
+        util.raiseNotDefined()
+
+
+def tinyMazeSearch(problem):
+    """
+    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
+    sequence of moves will be incorrect, so only use this for tinyMaze.
+    """
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    return  [s, s, w, s, w, w, s, w]
+
+def depthFirstSearch(problem):
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    e = Directions.EAST
+    n = Directions.NORTH
+    print("Start:", problem.getStartState())
+    a = problem.getStartState()
+    print(a, "\n")
+    print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
+    print("Start's successors:", problem.getSuccessors(problem.getStartState()))
+    print("Start's successors:", problem.getSuccessors((5,4)))
+
+
+
+    #from starting point s = problem.getStartState()
+    #chose one successor, chose its successor etc etc
+    #keep searching until exhausted or goal found
+    #stack, queue, priority queue from util functions avail
+    open = [problem.getStartState()]
+    closed = []
+    moves = []
+    parent = problem.getStartState()
+    routeMap = []
+    parentMap = {}
+    while (open != []):
+        current = open[len(open)-1]
+        parent = open[len(open)-1]
+        #if current is not a child -> remove last direction
+        if (problem.isGoalState(current)):
+            print("I hit the goalllllllll")
+            endGoal = current
+            break;
+            #return [s, s, w, s, w, w, s, w] #return the path to current
+        else:
+            children = problem.getSuccessors(current) #get the children of current
+            print("the children are ", children)
+            closed.append(open.pop()) #move the newly explored node to the closed section
+            print("open is ",open)
+            print("closed is ", closed)
+            for kid in children:  #do I need to reverse this
+                i=0
+                j =0
+                shouldAdd = True
+                while (open != [] and i<len(open)):
+                    if (open[i] == kid[0]):
+                        shouldAdd = False
+                    i = i+1
+                while (closed != [] and j<len(closed)):
+                    if (closed[j] == kid[0]):
+                        shouldAdd = False
+                    j=j+1
+                if shouldAdd:
+                    open.append(kid[0])
+                    print("tried to add a kid to open", open)
+                    parentMap[kid[0]] = current
+                if (i== len(children)-1):
+                    pass
+                    #routeMap.append(kid[1])
+    print("end goal is ", endGoal)
+    print("the problem statement end goal is ", problem.isGoalState(endGoal))
+    print(parentMap[endGoal], " is the parent of endGoal")
+    """
+    while (endGoal != problem.getStartState):
+        children = problem.getSuccessors(parentMap[endGoal])
+        for kids in children:
+            if endGoal == kids[0]:
+                routeMap.append(kids[1])
+        endGoal = parentMap[endGoal]
+    """
+
+
+
+
+
+    print("the route map is \n", routeMap, "\n")
+    return  [s, s, w, s, w, w, s, w]
+    """
+    open  = [start]
+    closed = []
+    while open != []
+        remove leftmost state from open [x]
+            if  x =  goal return successor
+            else
+                generate children of x
+                put x to closed
+                discard children of x if already open or closed
+                put remaining children on open
+            end
+        end
+        return fail
+    end
+    """
+
+
+    """
+    Search the deepest nodes in the search tree first.
+
+    Your search algorithm needs to return a list of actions that reaches the
+    goal. Make sure to implement a graph search algorithm.
+
+    To get started, you might want to try some of these simple commands to
+    understand the search problem that is being passed in:
+
+    print("Start:", problem.getStartState())
+    print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
+    print("Start's successors:", problem.getSuccessors(problem.getStartState()))
+
+    "*** YOUR CODE HERE ***"
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    return  [s, s, w, s, w, w, s, w]
+    print("Start:", problem.getStartState())
+    print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
+    print("Start's successors:", problem.getSuccessors(problem.getStartState()))
+
+    util.raiseNotDefined()
+    """
+def breadthFirstSearch(problem):
+    """Search the shallowest nodes in the search tree first."""
+    "*** YOUR CODE HERE ***"
+    util.raiseNotDefined()
+
+def uniformCostSearch(problem):
+    """Search the node of least total cost first."""
+    "*** YOUR CODE HERE ***"
+    util.raiseNotDefined()
+
+def nullHeuristic(state, problem=None):
+    """
+    A heuristic function estimates the cost from the current state to the nearest
+    goal in the provided SearchProblem.  This heuristic is trivial.
+    """
+    return 0
+
+def aStarSearch(problem, heuristic=nullHeuristic):
+    """Search the node that has the lowest combined cost and heuristic first."""
+    "*** YOUR CODE HERE ***"
+    util.raiseNotDefined()
+
+
+# Abbreviations
+bfs = breadthFirstSearch
+dfs = depthFirstSearch
+astar = aStarSearch
+ucs = uniformCostSearch
